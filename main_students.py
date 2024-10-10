8acac5063aeb1b6df775194e0c26734e0258f162
hartor8019 <107650063+hartor8019@users.noreply.github.com>
2024-10-09 22:00:30 -0500

    Add files via upload

--- main_students.py
+++ main_students.py
@@ -1,8 +1,8 @@
+#Se importan las librerias necesarias
 import pandas as pd
 import os
 
 # Cargar el archivo CSV
-# Asumiendo que el archivo CSV está en la misma carpeta que 'src'
 import os
 os.chdir('C:/Users/USUARIO/proyectosem1')
 df = pd.read_csv('src/IMDB top 1000.csv')
@@ -18,12 +18,15 @@ model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
 #Visualizo los nombre de las columnas
 print(df.columns)
 
+# Genera representaciones vectoriales (embeddings) para cada título en el DataFrame 'df'
+# utilizando el modelo pre-entrenado.
 embeddings = model.encode(df['Title'],batch_size=64,show_progress_bar=True)
 
+# Agrega una nueva columna 'embeddings' al DataFrame 'df' y almacena 
+# las representaciones vectoriales generadas en ella.
 df['embeddings'] = embeddings.tolist()
 
 ## Calculando la similitud usando la métrica de similitud por coseno
-
 def compute_similarity(example, query_embedding):
     embedding = example['embeddings'] 
     similarity = util.cos_sim(embedding, query_embedding).item()
@@ -31,7 +34,6 @@ def compute_similarity(example, query_embedding):
 
 
 ## Ejecuntando la búsqueda 
-
 from sentence_transformers import util
 
 def search_by_multiple_fields(df, query, fields=['Title', 'Genre']):
@@ -46,7 +48,6 @@ def search_by_multiple_fields(df, query, fields=['Title', 'Genre']):
   Returns:
     Un DataFrame ordenado por similitud.
   """
-
   query_embedding = model.encode([query])[0]
 
   df['combined_text'] = df[fields].apply(lambda x: ' '.join(x.dropna()), axis=1)
@@ -56,7 +57,6 @@ def search_by_multiple_fields(df, query, fields=['Title', 'Genre']):
 
 # Ejemplo de uso:
 results = search_by_multiple_fields(df, 'time travel sci-fi', fields=['Title', 'Genre'])
-#print(results.head()['Title'])
 
 #Imprimiendo el resultado de la busqueda
 print(results.head()[['Title', 'Genre']])
